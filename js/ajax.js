c210276ea262346f7f5847d375615f4d83eb2cf6
ruan65 <ruan65@gmail.com>
2017-12-01 01:07:15 +0300

    animals request

---
+++ js/ajax.js
@@ -0,0 +1,36 @@
+var btn = document.getElementById("btn")
+var jsonContainer = document.getElementById("animal_info")
+
+btn.addEventListener("click", function () {
+
+    var animalsUrl = "https://learnwebcode.github.io/json-example/animals-1.json"
+
+
+    var request = new XMLHttpRequest()
+
+    request.open('GET', animalsUrl)
+
+    request.onload = function (ev) {
+
+        var responseJson = JSON.parse(request.responseText)
+
+        console.log(responseJson[0])
+
+        renderHTML(responseJson)
+    }
+
+    request.send()
+})
+
+function renderHTML(json) {
+
+    var htmlString = ""
+
+    for (i = 0; i < json.length; i++) {
+
+        htmlString += "<p>" + json[i].name + "</p>"
+    }
+
+    jsonContainer.insertAdjacentHTML('beforeend', htmlString)
+
+}
